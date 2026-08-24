6076a2a23b848c20d0a6d1818f4cc292fe15be71
Pengju Zhang <xenthorpis2016@gmail.com>
2026-08-24 10:21:23 +0800

    feat: expose backtesting public API

--- src/condorpilot/__init__.py
+++ src/condorpilot/__init__.py
@@ -1,15 +1,24 @@
 """CondorPilot public package API."""
 
+from condorpilot.backtest import BacktestConfig, BacktestResult, ExitReason, run_backtest
+from condorpilot.execution import ExecutionConfig
+from condorpilot.history import OptionChainSnapshot
 from condorpilot.models import IronCondor, OptionQuote, OptionType, StrategyConfig
 from condorpilot.strategy import NoTradeError, build_iron_condor
 
 __all__ = [
+    "BacktestConfig",
+    "BacktestResult",
+    "ExecutionConfig",
+    "ExitReason",
     "IronCondor",
     "NoTradeError",
+    "OptionChainSnapshot",
     "OptionQuote",
     "OptionType",
     "StrategyConfig",
     "build_iron_condor",
+    "run_backtest",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
